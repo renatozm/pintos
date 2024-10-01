c203e1260b8f4ee8025142a335e1057c9500ff88
renato <renatiux@gmail.com>
2024-09-30 20:54:43 -0600

    Init

---
+++ src/devices/acpi.h
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   acpi.h
+ * Author: renato
+ *
+ * Created on 13 de mayo de 2016, 11:54 AM
+ */
+
+#ifndef ACPI_H
+#define ACPI_H
+
+void acpi_power_off(void);
+int acpi_init(void);
+int acpi_enable(void);
+
+#endif /* ACPI_H */
+
